2136fb062724b4db5ecfc0bf2fd909553b6d82dc
Tharushi Samaranayake <samarant@sheridancollege.ca>
2024-03-08 12:03:10 -0500

    made chnages to the macthing page

--- PlayPals/Controllers/HomeController.cs
+++ PlayPals/Controllers/HomeController.cs
@@ -43,9 +43,17 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult MatchingBrowser()
+    {
+        return View(); 
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+   
 }
+
+
