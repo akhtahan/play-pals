3723e7616592899439c76ba188538cd456cfe98c
Tharushi Samaranayake <samarant@sheridancollege.ca>
2024-03-08 19:12:02 -0500

    made chnages to the chat style page

--- PlayPals/Controllers/HomeController.cs
+++ PlayPals/Controllers/HomeController.cs
@@ -48,6 +48,11 @@ public class HomeController : Controller
         return View(); 
     }
 
+    public IActionResult Chatbox()
+    {
+        return View(); 
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
