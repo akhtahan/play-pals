6f9464f5f7adef86918e8906350afe64b3b76a8b
Tharushi Samaranayake <samarant@sheridancollege.ca>
2024-04-13 01:18:43 -0400

    Fixed Bugs

--- PlayPals/obj/Debug/net7/PlayPals.AssemblyInfo.cs
+++ PlayPals/obj/Debug/net7/PlayPals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayPals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e4e94e638e070dc759983fc805f6ec1dee9838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayPals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayPals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
