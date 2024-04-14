940076e9c99827a95245d873f122eafaff01a2ad
Tharushi Samaranayake <samarant@sheridancollege.ca>
2024-04-14 16:40:30 -0400

    Created the User class

---
+++ PlayPals/Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayPals.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public List<string> Interests { get; set; }
+    }
+}
